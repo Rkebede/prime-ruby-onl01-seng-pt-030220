585c9c0356fb7b6b95850b4e18e4ef750000183c
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-07 09:03:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,23 +1,30 @@
-def prime?(number)
-  remainder = number % 2 
-  prime_nums = [3, 5, 7, 11]
-  i = 2
+# def prime?(number)
+#   remainder = number % 2 
+#   prime_nums = [3, 5, 7, 11]
+#   i = 2
   
-  while i < number 
-    prime_nums.each do |num|
-      num % 2 
-  end
+#   while i < number 
+#     prime_nums.each do |num|
+#       num % 2 
+#   end
 
       
   
-  if i < 2 || (remainder == 0 && number != 2)
-    false
-  else
-    true 
-  end 
+#   if i < 2 || (remainder == 0 && number != 2)
+#     false
+#   else
+#     true 
+#   end 
+# end
+
+def prime?(integer)
+    i = 2
+    while i < integer
+        return false if integer % i == || integer < 1
+        i += 1
+    end
+    true
 end
-
-
 
 
 
