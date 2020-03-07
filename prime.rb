e8104177f735a08aeb8f8c66f2e97bb87eb06745
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-07 09:21:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,30 +1,16 @@
-# def prime?(number)
-#   remainder = number % 2 
-#   prime_nums = [3, 5, 7, 11]
-#   i = 2
-  
-#   while i < number 
-#     prime_nums.each do |num|
-#       num % 2 
-#   end
-
-      
-  
-#   if i < 2 || (remainder == 0 && number != 2)
-#     false
-#   else
-#     true 
-#   end 
-# end
-
-
-
-
-
-
-
-
-
-
-# To prove whether a number is a prime number, first try dividing it by 2, and see if you get a whole number. If you do, it can't be a prime number. If you don't get a whole number, next try dividing it by prime numbers: 3, 5, 7, 11 (9 is divisible by 3) and so on, always dividing by a prime number
-
+def prime?(number)
+  remainder = number % 2
+  if number < 2 || (remainder == 0 && number != 2)
+    false
+  else
+    count = number - 1 
+    while count > 0
+      if number % count == 0
+        return false
+      else 
+        count -= 1
+      end 
+    end
+    true
+  end
+end 
