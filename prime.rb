90b90ea810ca354e09894c37c20ead53f68775de
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-07 07:39:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,11 @@
-def prime?(number)
-  number == 
+def prime?(numbers)
+  not_prime = numbers.map { |number| number % 2 }
+  end 
 end
 
+
+
+
+
+
+To prove whether a number is a prime number, first try dividing it by 2, and see if you get a whole number. If you do, it can't be a prime number. If you don't get a whole number, next try dividing it by prime numbers: 3, 5, 7, 11 (9 is divisible by 3) and so on, always dividing by a prime number
