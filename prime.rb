d9a8f65635dd79dab54ff0dc371a282514cdef8d
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-07 09:31:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(number)
+  remainder = number % 2
+  if number < 2 || (remainder == 0 && number != 2)
+    false
+  else
+    count = number - 1 
+    while count > 2
+      if number % count == 0
+        return false
+      else 
+        count -= 1
+      end 
+    end
+    true
+  end
+end 
